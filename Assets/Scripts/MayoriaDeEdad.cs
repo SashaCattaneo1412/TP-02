e51e1cde5ad15cab9fd007fa5376b6a23fe74cf3
SashaCattaneo1412 <101577464+SashaCattaneo1412@users.noreply.github.com>
2022-04-24 16:16:50 -0300

    Finished TP_02

---
+++ Assets/Scripts/MayoriaDeEdad.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MayoriaDeEdad : MonoBehaviour
+{
+    public int edadUsuario;
+    public Text miTexto;
+
+    void Start()
+    {
+        
+        if ( edadUsuario >= 18)
+        {
+            miTexto.text = "Es mayor de Edad";
+            Debug.Log("Es mayor de Edad");
+        }
+        else
+        {
+            miTexto.text = "No es mayor de Edad";
+            Debug.Log("No es mayor de Edad");
+        }
+
+    }
+}
---
+++ Assets/scripts 1/ValidarContraseña.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ValidarContraseña : MonoBehaviour
+{
+    string contraseñaCorrecta;
+    string contraseñaUsuario;
+    public Text ingresoUsuario;
+    public Text textoMsj;
+    public GameObject cartelitoMsj;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cartelitoMsj.SetActive(false);
+        contraseñaCorrecta = "12345";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void validarContraseña()
+    {
+        contraseñaUsuario = ingresoUsuario.text;
+        if(contraseñaUsuario == contraseñaCorrecta)
+        {
+            cartelitoMsj.SetActive(true);
+            textoMsj.text="Bienvenido";
+            Debug.Log("Bienvenido");
+        }
+        else
+        {
+            cartelitoMsj.SetActive(true);
+            textoMsj.text = "Contraseña Incorrecta";
+            Debug.Log("Contraseña Incorrecta");
+
+        }
+    }
+}
